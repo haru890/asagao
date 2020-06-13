8cca19506e68f630ab739a9951aa9b787feff114
haru90 <nakamigawa.h@gmail.com>
2020-06-13 13:00:34 +0900

    add column for save hash value

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_131031) do
+ActiveRecord::Schema.define(version: 2020_06_13_035641) do
 
   create_table "members", force: :cascade do |t|
     t.integer "number", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_30_131031) do
     t.boolean "administrator", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
