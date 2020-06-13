e173f43687ae58536b1cab8d6d9151608a70b345
haru90 <nakamigawa.h@gmail.com>
2020-06-13 14:31:36 +0900

    create login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def create
     member = Member.find_by(name: params[:name])
     if member&.authenticate(params[:password])
-      session[:member_id] = member.member_id
+      session[:member_id] = member.id
     else
       flash.alert = "名前とパスワードが一致しません"
     end
-      redirect_to :root
+    redirect_to :root
   end
 
   def destroy
