70071ec8bc52ea20d76fcc405b24cc2598cd5765
haru90 <nakamigawa.h@gmail.com>
2020-06-20 18:37:26 +0900

    create pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,27 @@ class ArticlesController < ApplicationController
   # 記事一覧
   def index
     @articles = Article.order(released_at: :desc)
+
+    @articles = @articles.open_to_the_public unless current_member
+
+    unless current_member&.administrator?
+      @articles = @articles.visible
+    end
+
+    @articles = @articles.page(params[:page]).per(5)
   end
 
   # 記事詳細
   def show
+    articles = Article.all
+
     articles = articles.open_to_the_public unless current_member
+
+    unless current_member&.administrator?
+      articles = articles.visible
+    end
+
+    @article = articles.find(params[:id])
   end
 
   # 新規登録フォーム
