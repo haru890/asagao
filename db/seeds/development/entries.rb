cb47fe0447f31a3c429abab434797eb396334b79
haru90 <nakamigawa.h@gmail.com>
2020-06-26 23:30:46 +0900

    create like function

--- db/seeds/development/entries.rb
+++ db/seeds/development/entries.rb
@@ -1,25 +1,26 @@
 body =
-  "Morning Gloryが4対2でSunflowerに勝利。\n\n" +
-  "2回表、6番渡辺の二塁打から7番山田、8番高橋の連続タイムリーで2点先制。" +
-  "9回表、ランナー一二塁で2番田中の二塁打で2点を挙げ、ダメを押しました。\n\n" +
-  "投げては初先発の山本が7回を2失点に抑え、伊藤、中村とつないで逃げ切りました。"
+  "今晩は久しぶりに神宮で野球観戦。内野B席の上段に着席。\n\n" +
+  "先発はヤクルトがブキャナン、広島はジョンソン。" +
+  "2回裏に中村選手のセーフティスクイズなどでヤクルトが3点を先取。" +
+  "そして、8回裏には代打・荒木選手がレフトスタンドへ2号満塁ホームラン。\n\n" +
+  "ブキャナン投手の今季初完封を見届けて、気分良く家路に着きました。"
 
-0.upto(9) do |idx|
-  Article.create(
-    title: "練習試合の結果#{idx}",
-    body: body,
-    released_at: 8.days.ago.advance(days: idx),
-    expired_at: 2.days.ago.advance(days: idx),
-    member_only: (idx % 3 == 0)
-  )
-end
+%w(Taro Jiro Hana).each do |name|
+  member = Member.find_by(name: name)
+  0.upto(9) do |idx|
+    entry = Entry.create(
+      author: member,
+      title: "野球観戦#{idx}",
+      body: body,
+      posted_at: 10.days.ago.advance(days: idx),
+      status: %w(draft member_only public)[idx % 3]
+    )
 
-0.upto(29) do |idx|
-  Article.create(
-    title: "Article#{idx+10}",
-    body: "blah, blah, blah...",
-    released_at: 100.days.ago.advance(days: idx),
-    expired_at: nil,
-    member_only: false
-  )
+    if idx == 7 || idx == 8
+      %w(John Mike Sophy).each do |name2|
+        voter = Member.find_by(name: name2)
+        voter.voted_entries << entry
+      end
+    end
+  end
 end
